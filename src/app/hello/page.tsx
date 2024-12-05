411e5722587ee31d5be81691ca82bd9d9e9fd129
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-05 23:24:01 +0530

    initial commit

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,5 @@
+export default function Hello(){
+  return (
+    <h1 className="text-3xl m-5 text-center">Hello from next js</h1>
+  )
+}
---
+++ src/app/hello/scatch/page.tsx
@@ -0,0 +1,6 @@
+export default function Scatch(){
+  return (
+    <h1> hello from scatch</h1>
+
+  )
+  }
