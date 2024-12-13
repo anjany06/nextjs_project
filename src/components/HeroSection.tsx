a1614ac5f4500045ea95388dc091a7eab3a4f946
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-13 22:41:55 +0530

    `feat: updated HeroSection, Navbar, and package files with new icons and links`

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
+import { FaXTwitter } from "react-icons/fa6";
 
 function HeroSection() {
   return (
@@ -16,23 +19,50 @@ function HeroSection() {
         fill="white"
       />
       <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+        <h1 className="mt-24 md:mt-8 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Hello I'm Anjany Pandey
         </h1>
-        <p className="mt-5 font-normal text-base md:text-xl text-neutral-300 max-w-lg mx-auto">
-          Dive into our comprehensive music course and transform your musical
-          journey today. Whether you' re a beginner or looking to refine your
-          skills, join to unlock your true potential.
+        <p className="mt-5 font-normal text-base md:text-xl text-neutral-300 max-w-xl mx-auto">
+          A Passionate front-end developer with a strong
+          foundation in C++ and a growing expertise in React.js, currently
+          pursuing BCA from VIPS-TC. I enjoy turning complex problems into
+          simple, beautiful, and intuitive designs.
         </p>
-        <div className="mt-6">
-          <Link href={"/courses"}>
+        <div className="mt-6 flex md:flex-row flex-col justify-center items-center gap-8">
+          <div>
+            <Link href={"/courses"}>
+              <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              >
+                Explore More
+              </Button>
+            </Link>
+          </div>
+
+          <div className="flex gap-5">
             <Button
-              borderRadius="1.75rem"
+              borderRadius="0rem"
               className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              style={{ width: "50px", height: "50px" }}
             >
-              Explore More
+              <FaGithub style={{ width: "30px", height: "30px" }} />
             </Button>
-          </Link>
+            <Button
+              borderRadius="0rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              style={{ width: "50px", height: "50px" }}
+            >
+              <FaLinkedin style={{ width: "30px", height: "30px" }} />
+            </Button>
+            <Button
+              borderRadius="0rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              style={{ width: "50px", height: "50px" }}
+            >
+              <FaXTwitter style={{ width: "30px", height: "30px" }} />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,47 @@
-'use client';
+"use client";
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 // link tag hamare a tag ki trah page reloads ni krte
 import Link from "next/link";
 
-function Navbar({ className }: { className?: string })  {
+function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-    {/* so menu ko koi na koi children chaiye hota h wrna errr */}
-      <Menu setActive={setActive}> 
-      <Link href = {"/"}>
-      <MenuItem setActive={setActive} active={active} item = "Home">
-      {/* <div className="flex flex-col space-y-4 text-sm">
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      {/* so menu ko koi na koi children chaiye hota h wrna errr */}
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home">
+            {/* <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/courses">All Courses</HoveredLink>
             <HoveredLink href="/interface-design">Interface Design</HoveredLink>
             <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
             <HoveredLink href="/branding">Branding</HoveredLink>
           </div> */}
-      </MenuItem>
-      </Link  >
-      <MenuItem setActive={setActive} active={active} item = "Our Coures">
-      <div className="flex flex-col space-y-4 text-sm">
-      <HoveredLink href="/courses">All Courses</HoveredLink>
-      <HoveredLink href="/s">Basic Music Theory</HoveredLink>
-      <HoveredLink href="/courses">Advanced Composition</HoveredLink>
-      <HoveredLink href="/courses">Songwriting</HoveredLink>
-      <HoveredLink href="/courses">Music Production</HoveredLink>
-      </div>
-      </MenuItem>
-      <Link href = {"/contact"}>
-      <MenuItem setActive={setActive} active={active} item = "Contact Us">
-      </MenuItem>
-      </Link>
+          </MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Projects">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/s">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
       </Menu>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
