8b64d6256275960c67e320c09714b998b2db66eb
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-20 21:07:21 +0530

    `Added image domains to Next.js config and updated Contact component to use new image URLs`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import SkillsCard from "@/components/SkillsCard";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <SkillsCard />
       <UpcomingWebinars />
       <Contact />
+      <Footer />
     </main>
   );
 }
